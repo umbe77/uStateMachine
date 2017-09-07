1168bd8d37ef4d3c2d6e1793816ce1c511e8f695
Roberto Ughi <umbe77@gmail.com>
2017-09-08 00:48:40 +0200

    incapsulae wf-engine in uRestServer MiddleWare statemachines list controller

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const path = require('path')
 const {rest} = require('urestserver')
 const settings = require('./lib/utilities/settings').getSettings()
 
+const cache = require('./lib/cache/cacheManager')
+const persistance = require('./lib/persistance/persistance-manager')
+
+const wfEngine = require('./lib/wf-engine')(cache, persistance)
+
 rest.createRest({
-    port: settings.port,
-    controllerPath: path.resolve('./lib/restServer')
+    port: settings.port
 })
-.loadControllers(path.resolve('./lib/restServer'))
+.loadControllers(path.resolve('./lib/restServer/apicontrollers'))
+.addMiddleware((svc) => {
+    svc.context.engine = wfEngine
+})
 .run()
 
