d16b49948f053c7da03adb086f62a7b299201e43
Roberto Ughi <umbe77@gmail.com>
2017-09-20 01:21:13 +0200

    add cluster support add error control

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const path = require('path')
-const {rest} = require('urestserver')
+const {
+    rest
+} = require('urestserver')
 const settings = require('./lib/utilities/settings').getSettings()
 
 const cache = require('./lib/cache/cacheManager')
@@ -7,12 +9,40 @@ const persistance = require('./lib/persistance/persistance-manager')
 
 const wfEngine = require('./lib/wf-engine')(cache, persistance)
 
-rest.createRest({
-    port: settings.port
-})
-.loadControllers(path.resolve('./lib/restServer/apicontrollers'))
-.addMiddleware((svc) => {
-    svc.context.engine = wfEngine
-})
-.run()
+const cluster = require('cluster')
+const os = require('os')
 
+const numCpus = os.cpus().length
+
+if (cluster.isMaster) {
+
+    console.log(`starting cluester on ${numCpus} workers`)
+
+    for (let i = 0; i < numCpus; ++i) {
+        cluster.fork()
+    }
+
+    cluster.on('online', (worker) => {
+        console.log(`Worker ${worker.process.pid} is online`)
+    })
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker died with [CODE]: ${code} -- [SIGNAL]: ${signal}`)
+        console.log('restarting worker')
+        cluster.fork()
+    })
+
+} else {
+    rest.createRest({
+            port: settings.port
+        })
+        .loadControllers(path.resolve('./lib/restServer/apicontrollers'))
+        .addMiddleware((svc) => {
+            svc.context.engine = wfEngine
+        })
+        .addMiddleware((svc) => {
+            console.log(`${process.pid} -- [${svc.req.url}]`)
+        })
+        .run()
+    console.log(`process started on pid: ${process.pid}`)
+}
